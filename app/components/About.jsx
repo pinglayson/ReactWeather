c2bd8604befcd9075070636db3d9d678f48d5217
pinglayson <pinglayson@gmail.com>
2017-02-16 13:45:59 +0800

    style page about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,12 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to about page</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is app is created for learning React.<br/>
+        Course Details <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/">here</a><br/>
+        Link to git repository <a href="https://github.com/pinglayson/ReactWeather">here</a>
+      </p>
     </div>
   )
 }
